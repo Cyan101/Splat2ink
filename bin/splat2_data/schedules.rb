bb3ba39b2bcc72b8c078c979cded10308ac64efd
Cyan/Jos <crazyandroid101@gmail.com>
2018-04-22 01:29:10 +0800

    Cut down the code for grabbing stage info

--- bin/splat2_data/schedules.rb
+++ bin/splat2_data/schedules.rb
@@ -10,19 +10,19 @@ schedules = JSON.parse(resp.body)
 # Save 4 "waves" of each mode
 %w[regular gachi league].each do |x|
   4.times do |i|
-    to_save = {game_type: "", stage_1: {}, stage_2: {}, time_start: "", time_end: "" }
+    to_save = { game_type: '', stage_1: {}, stage_2: {}, time_start: '', time_end: '' }
     data = schedules[x][i]
     to_save[:game_type] = data['rule']['name']
-    to_save[:stage_1][:image] = data['stage_a']['image']
-    to_save[:stage_1][:name] = data['stage_a']['name']
-    to_save[:stage_2][:image] = data['stage_b']['image']
-    to_save[:stage_2][:name] = data['stage_b']['name']
+    %w[image name].each do |p|
+      to_save[:stage_1][p.to_sym] = data['stage_a'][p]
+      to_save[:stage_2][p.to_sym] = data['stage_b'][p]
+    end
     to_save[:time_start] = data['start_time']
     to_save[:time_end] = data['end_time']
     $splat2_data[:schedules][x.to_sym][i] = to_save
     %i[stage_1 stage_2].each do |p| # Save the images for the maps locally
       image_url = to_save[p][:image]
-      File.write '../../public/' + image_url, open(BASE_URL + image_url).read unless File.exists?('../../public/' + image_url)
+      File.write '../../public/' + image_url, open(BASE_URL + image_url).read unless File.exist?('../../public/' + image_url)
     end
   end
 end
