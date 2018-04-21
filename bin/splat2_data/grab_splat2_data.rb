cc60c54002412d41b176bff46552aeacb93733bc
Cyan/Jos <crazyandroid101@gmail.com>
2018-04-22 00:37:54 +0800

    small comment added

--- bin/splat2_data/grab_splat2_data.rb
+++ bin/splat2_data/grab_splat2_data.rb
@@ -5,7 +5,7 @@ require 'json'
 @splatnet2_token = YAML.load(File.read('../../config.yaml'))[:splatnet2_cookie]
 BASE_URL = 'https://app.splatoon2.nintendo.net'
 
-$splat2_data = {
+$splat2_data = { # Create a skeleton hash
   schedules: {
     regular: {},
     gachi: {}, # This is deleted and renamed to :ranked
